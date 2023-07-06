50be3c5f5d53212358d509870ad80b5f6b6ba616
githukelvin <kelvingithu019@gmail.com>
2023-07-06 18:18:39 +0300

    day two is success

--- githukelvin/src/components/HelloWorld.vue
+++ githukelvin/src/components/HelloWorld.vue
@@ -41,7 +41,7 @@ export default {
   },
   data () {
     return {
-      srcData,
+      srcData: srcData,
       r: 'Welcome to Your Vue.js App'
     }
   }
---
+++ githukelvin/src/components/PageHome.vue
@@ -0,0 +1,37 @@
+<script >
+import srcData from '@/data.json'
+
+export default {
+  data () {
+    return {
+      srcData: srcData,
+      threads: srcData.threads,
+      post: srcData.posts,
+      users: srcData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.post.find(p => p.id === postId)
+    },
+    userById (userid) {
+      return this.users.find(u => u.id === userid)
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>Hello from homepage</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+    <h1>{{ userById(thread.userId).name }}</h1>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
